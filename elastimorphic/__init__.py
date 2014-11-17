d9bb67b1034eb15bdc267fbdf37cf6443cff7e5a
Adam Wentz <awentz@theonion.com>
2014-11-17 12:10:06 -0600

    Bumped version 0.1.7

--- elastimorphic/__init__.py
+++ elastimorphic/__init__.py
@@ -1,6 +1,6 @@
 from .base import Indexable, PolymorphicIndexable, SearchManager  # noqa
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __all__ = [PolymorphicIndexable, SearchManager]
 
 default_app_config = "elastimorphic.apps.ElastimorphicConfig"
