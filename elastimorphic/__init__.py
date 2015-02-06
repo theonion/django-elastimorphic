3ecdb386df491dccb86a46dae46201cf58f6b8ec
Adam Wentz <awentz@theonion.com>
2015-02-06 16:12:31 -0600

    Bumping version to 0.2.0. Dropped django <= 1.5 compatibility

--- elastimorphic/__init__.py
+++ elastimorphic/__init__.py
@@ -1,6 +1,6 @@
 from .base import Indexable, PolymorphicIndexable, SearchManager  # noqa
 
-__version__ = "0.1.8"
+__version__ = "0.2.0"
 __all__ = [PolymorphicIndexable, SearchManager]
 
 default_app_config = "elastimorphic.apps.ElastimorphicConfig"
