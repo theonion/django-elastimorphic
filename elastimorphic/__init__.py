8273637863b88c2300d6372bf4753b4f865e3028
Adam Wentz <awentz@theonion.com>
2015-02-06 15:35:25 -0600

    Releasing v0.1.8

--- elastimorphic/__init__.py
+++ elastimorphic/__init__.py
@@ -1,6 +1,6 @@
 from .base import Indexable, PolymorphicIndexable, SearchManager  # noqa
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 __all__ = [PolymorphicIndexable, SearchManager]
 
 default_app_config = "elastimorphic.apps.ElastimorphicConfig"
